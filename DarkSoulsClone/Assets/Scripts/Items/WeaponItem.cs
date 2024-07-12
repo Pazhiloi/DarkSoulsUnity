8ef23225aff6b388128e6960dae6a954e96f47b8
Miroslav Miroslavov <martinsen06@gmail.com>
2024-07-12 14:47:53 +0300

    13 Weapon IDLE Animations

--- DarkSoulsClone/Assets/Scripts/Items/WeaponItem.cs
+++ DarkSoulsClone/Assets/Scripts/Items/WeaponItem.cs
@@ -1,15 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-namespace SG{
-[CreateAssetMenu(menuName = "Items/Weapon Item")]
-public class WeaponItem : Item
+namespace SG
 {
-   public GameObject modelPrefab;
-   public bool isUnarmed;
-   [Header("One Handed Attack Animations")]
-   public string oh_light_attack_01;
-   public string oh_light_attack_02;
-   public string oh_heavy_attack_01;
-}
+  [CreateAssetMenu(menuName = "Items/Weapon Item")]
+  public class WeaponItem : Item
+  {
+    public GameObject modelPrefab;
+    public bool isUnarmed;
+
+    [Header("Idle Animations")]
+
+    public string right_hand_idle;
+    public string left_hand_idle;
+    
+    [Header("One Handed Attack Animations")]
+    public string oh_light_attack_01;
+    public string oh_light_attack_02;
+    public string oh_heavy_attack_01;
+  }
 }
--- DarkSoulsClone/Assets/Scripts/Items/WeaponSlotManager.cs
+++ DarkSoulsClone/Assets/Scripts/Items/WeaponSlotManager.cs
@@ -12,7 +12,10 @@ public class WeaponSlotManager : MonoBehaviour
     DamageCollider leftHandDamageCollider;
     DamageCollider rightHandDamageCollider;
 
+    Animator animator;
+
     private void Awake() {
+      animator = GetComponent<Animator>();
       WeaponHolderSlot[] weaponHolderSlots = GetComponentsInChildren<WeaponHolderSlot>();
       foreach (WeaponHolderSlot weaponSlot in weaponHolderSlots)
       {
@@ -32,10 +35,30 @@ public class WeaponSlotManager : MonoBehaviour
       {
         leftHandSlot.LoadWeaponModel(weaponItem);
         LoadLeftWeaponDamageCollider();
+        #region  Handle Left Weapon Item Idle Animations
+
+        if (weaponItem != null)
+        {
+          animator.CrossFade(weaponItem.left_hand_idle, 0.2f);
+        }else{
+          animator.CrossFade("Left Arm Empty", 0.2f);
+        }
+
+        #endregion
       }
       else{
         rightHandSlot.LoadWeaponModel(weaponItem);
         LoadRightWeaponDamageCollider();
+        #region  Handle Right  Weapon Item Idle Animations
+        if (weaponItem != null)
+        {
+          animator.CrossFade(weaponItem.right_hand_idle, 0.2f);
+        }
+        else
+        {
+          animator.CrossFade("Right Arm Empty", 0.2f);
+        }
+        #endregion
       }
     }
 
