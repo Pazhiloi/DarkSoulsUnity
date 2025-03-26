803f82c7d5387dd7ef2bd17a2f6e9edfd31e872c
Miroslav Miroslavov <martinsen06@gmail.com>
2025-03-26 12:56:08 +0200

    67 fog walls

--- DarkSoulsClone/Assets/Scripts/Interactions/PassThroughFogWall.cs
+++ DarkSoulsClone/Assets/Scripts/Interactions/PassThroughFogWall.cs
@@ -11,11 +11,11 @@ namespace SG
         private void Awake() {
       worldEventManager = FindObjectOfType<WorldEventManager>();
         }
-
     public override void Interact(PlayerManager playerManager)
     {
       base.Interact(playerManager);
       playerManager.PassThroughFogWallInteraction(transform);
+      worldEventManager.ActivateBossFight();
     }
   }
 }
