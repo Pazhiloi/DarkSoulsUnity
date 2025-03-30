8698ee48d2b1608c67b506218b21a39b955725e7
Miroslav Miroslavov <martinsen06@gmail.com>
2025-03-31 02:29:45 +0300

    Start 77

---
+++ DarkSoulsClone/Assets/Scripts/Items/BombConsumeableItem.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+
+namespace SG
+{
+  [CreateAssetMenu(menuName = "Items/Consumables/Bomb Item")]
+  public class BombConsumeableItem : ConsumableItem
+    {
+    [Header("Velocity")]
+    public int upwardVelocity = 50;
+    public int forwardVelocity = 50;
+    public int bombMass = 1;
+
+    [Header("Live Bomb Model")]
+    public GameObject liveBombModel;
+
+    [Header("Base Damage")]
+    public int baseDamage = 200;
+
+    public override void AttemptToConsumeItem(PlayerAnimatorManager playerAnimatorManager, PlayerWeaponSlotManager weaponSlotManager, PlayerEffectsManager playerEffectsManager)
+    {
+      if (currentItemAmount > 0)
+      {
+        playerAnimatorManager.PlayTargetAnimation(consumeAnimation, true);
+        GameObject bombModel = Instantiate(itemModel, weaponSlotManager.rightHandSlot.transform.position, Quaternion.identity, weaponSlotManager.rightHandSlot.transform);
+        playerEffectsManager.instantiatedFXModel = bombModel;
+      }
+      else
+      {
+        playerAnimatorManager.PlayTargetAnimation("Shrug", true);
+      }
+    }
+  }
+}
