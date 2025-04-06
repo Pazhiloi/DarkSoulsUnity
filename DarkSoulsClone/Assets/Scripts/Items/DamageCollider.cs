61e9770ab67afee35451446d0c50e739d7bea44d
Miroslav Miroslavov <martinsen06@gmail.com>
2025-04-06 03:16:49 +0300

    Start 83

--- DarkSoulsClone/Assets/Scripts/Items/DamageCollider.cs
+++ DarkSoulsClone/Assets/Scripts/Items/DamageCollider.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.TextCore.Text;
 namespace SG
 {
   public class DamageCollider : MonoBehaviour
@@ -20,6 +21,10 @@ namespace SG
     public int magicDamage;
     public int lightningDamage;
     public int darkDamage;
+
+    bool shieldHasBeenHit;
+    bool hasBeenParried;
+    string currentDamageAnimation;
     protected virtual void Awake()
     {
       damageCollider = GetComponent<Collider>();
@@ -41,6 +46,9 @@ namespace SG
     {
       if (other.tag == "Character")
       {
+        shieldHasBeenHit = false;
+        hasBeenParried = false;
+
         CharacterStatsManager enemyStats = other.GetComponent<CharacterStatsManager>();
         CharacterManager enemyManager = other.GetComponent<CharacterManager>();
         CharacterEffectsManager enemyEffects = other.GetComponent<CharacterEffectsManager>();
@@ -48,46 +56,38 @@ namespace SG
 
         if (enemyManager != null)
         {
-          if (enemyStats.teamIDNumber == teamIDNumber)return;
-          if (enemyManager.isParrying)
-          {
-            characterManager.GetComponentInChildren<CharacterAnimatorManager>().PlayTargetAnimation("Parried", true);
-            return;
-          }
-          else if (shield != null && enemyManager.isBlocking)
-          {
-            float physicalDamageAfterBlock = physicalDamage - (physicalDamage * shield.blockingPhysicalDamageAbsorption) / 100;
-            float fireDamageAfterBlock = fireDamage - (fireDamage * shield.blockingFireDamageAbsorption) / 100;
-            if (enemyStats != null)
-            { enemyStats.TakeDamage(Mathf.RoundToInt(physicalDamageAfterBlock), 0, "Block Guard"); }
-            // REFACTOR
-            return;
-          }
+          if (enemyStats.teamIDNumber == teamIDNumber) return;
+
+          CheckForParry(enemyManager);
+          CheckForBlock(enemyManager, enemyStats, shield);
+
+
         }
 
         if (enemyStats != null)
         {
           if (enemyStats.teamIDNumber == teamIDNumber) return;
+          if (hasBeenParried)return;
+          if (shieldHasBeenHit)return;
+
           enemyStats.poiseResetTimer = enemyStats.totalPoiseResetTime;
           enemyStats.totalPoiseDefence = enemyStats.totalPoiseResetTime - poiseBreak;
 
           Vector3 contactPoint = other.gameObject.GetComponent<Collider>().ClosestPointOnBounds(transform.position);
+          float directionHitFrom = (Vector3.SignedAngle(characterManager.transform.forward, enemyManager.transform.forward, Vector3.up));
+          ChooseWhichDirectionDamageCameFrom(directionHitFrom);
           enemyEffects.PlayBloodSplatterFX(contactPoint);
 
           if (enemyStats.totalPoiseDefence > poiseBreak)
           {
             enemyStats.TakeDamageNoAnimation(physicalDamage, 0);
-            //REFACTOR
           }
           else
           {
-            enemyStats.TakeDamage(physicalDamage, 0);
-            //REFACTOR
+            enemyStats.TakeDamage(physicalDamage, 0, currentDamageAnimation);
           }
         }
       }
-
-
 
       if (other.tag == "Illusionary Wall")
       {
@@ -96,5 +96,52 @@ namespace SG
         illusionaryWall.wallHasBeenHit = true;
       }
     }
+
+    protected virtual void CheckForParry(CharacterManager enemyManager)
+    {
+      if (enemyManager.isParrying)
+      {
+        characterManager.GetComponentInChildren<CharacterAnimatorManager>().PlayTargetAnimation("Parried", true);
+        hasBeenParried = true;
+      }
+    }
+
+    protected virtual void CheckForBlock(CharacterManager enemyManager, CharacterStatsManager enemyStats, BlockingCollider shield)
+    {
+      if (shield != null && enemyManager.isBlocking)
+      {
+        float physicalDamageAfterBlock = physicalDamage - (physicalDamage * shield.blockingPhysicalDamageAbsorption) / 100;
+        float fireDamageAfterBlock = fireDamage - (fireDamage * shield.blockingFireDamageAbsorption) / 100;
+
+        if (enemyStats != null)
+        { enemyStats.TakeDamage(Mathf.RoundToInt(physicalDamageAfterBlock), 0, "Block Guard"); }
+        shieldHasBeenHit = true;
+      }
+    }
+
+    protected virtual void ChooseWhichDirectionDamageCameFrom(float direction)
+    {
+      if (direction >= 145 && direction <= 180)
+      {
+        currentDamageAnimation = "Damage_Forward_01";
+      }
+      else if (direction <= -145 && direction >= -180)
+      {
+        currentDamageAnimation = "Damage_Forward_01";
+      }
+      else if (direction >= -45 && direction <= 45)
+      {
+        currentDamageAnimation = "Damage_Back_01";
+      }
+      else if (direction >= -144 && direction <= -45)
+      {
+        currentDamageAnimation = "Damage_Right_01";
+      }
+      else if (direction >= 45 && direction <= 144)
+      {
+        currentDamageAnimation = "Damage_Left_01";
+      }
+    }
+
   }
 }
